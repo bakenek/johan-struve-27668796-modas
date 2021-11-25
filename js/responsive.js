b749498bbc457a8862cdf553408ef959b409e501
bakenek <jdstruve1@urbe.edu.ve>
2021-11-24 22:59:42 -0400

    Initial commit

---
+++ js/responsive.js
@@ -0,0 +1,18 @@
+
+addEventListener('DOMContentLoaded', () => {
+	
+   const btn_menu = document.querySelector('.btn_menu')
+   
+   
+   
+   if (btn_menu) {
+	   
+       btn_menu.addEventListener('click', () => {
+		   
+           const menu_items = document.querySelector ('.menu2')
+		   
+           menu_items.classList.toggle('show')
+       })
+   }
+})
+
